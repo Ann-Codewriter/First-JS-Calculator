4bc74d4840e06b095b7be1d1c8a9b5988a7431ff
Ann-Codewriter <qantoinnette23@gmail.com>
2025-09-05 18:57:22 -0700

    My first JS Calculator.

---
+++ js/script.js
@@ -0,0 +1,43 @@
+//Task 1 - Creating Variables
+let name = "Ann"; //string
+console.log(name);
+
+let age = 20; //number
+console.log(age);
+
+let likesCoding = true; //Boolean
+console.log(likesCoding);
+
+
+//Task 2 - Operators
+
+//Addition
+let num1 = 10; 
+let num2 = 8;
+console.log(num1 + num2);
+
+//Difference
+let num1 = 10; 
+let num2 = 8;
+console.log(num1 - num2);
+
+//Product
+let num1 = 10; 
+let num2 = 8;
+console.log(num1 * num2);
+
+//Quotient
+let num1 = 10; 
+let num2 = 8;
+console.log(num1 / num2);
+
+
+//Task 3 - String concatenation
+let info1 = "my name is"
+let info2 = "I am"
+let name = "Ann";
+let age = 20;
+let question = "Do I like coding?"
+let likesCoding = true;
+console.log("Hello, " + info1 + " " + name + ","+ " " + info2 + " " + age + " " + "years old. " + question + " " + likesCoding)
+
