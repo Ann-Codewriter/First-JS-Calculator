86bb95a102970a489c889c083f1bf5fe041767ed
Ann-Codewriter <qantoinnette23@gmail.com>
2025-09-16 14:44:39 -0700

    Add a condition.

--- js/script.js
+++ js/script.js
@@ -1,43 +1,61 @@
-//Task 1 - Creating Variables
-let name = "Ann"; //string
-console.log(name);
+// //Task 1 - Creating Variables
+// let name = "Ann"; //string
+// console.log(name);
 
-let age = 20; //number
-console.log(age);
+// let age = 20; //number
+// console.log(age);
 
-let likesCoding = true; //Boolean
-console.log(likesCoding);
+// let likesCoding = true; //Boolean
+// console.log(likesCoding);
 
 
-//Task 2 - Operators
+// //Task 2 - Operators
 
-//Addition
-let num1 = 10; 
-let num2 = 8;
-console.log(num1 + num2);
+// //Addition
+// let num1 = 10; 
+// let num2 = 8;
+// console.log(num1 + num2);
 
-//Difference
-let num1 = 10; 
-let num2 = 8;
-console.log(num1 - num2);
+// //Difference
+// let num1 = 10; 
+// let num2 = 8;
+// console.log(num1 - num2);
 
-//Product
-let num1 = 10; 
-let num2 = 8;
-console.log(num1 * num2);
+// //Product
+// let num1 = 10; 
+// let num2 = 8;
+// console.log(num1 * num2);
 
-//Quotient
-let num1 = 10; 
-let num2 = 8;
-console.log(num1 / num2);
+// //Quotient
+// let num1 = 10; 
+// let num2 = 8;
+// console.log(num1 / num2);
 
 
 //Task 3 - String concatenation
-let info1 = "my name is"
-let info2 = "I am"
-let name = "Ann";
-let age = 20;
-let question = "Do I like coding?"
-let likesCoding = true;
-console.log("Hello, " + info1 + " " + name + ","+ " " + info2 + " " + age + " " + "years old. " + question + " " + likesCoding)
+// let info1 = "my name is"
+// let info2 = "I am"
+// let name = "Ann";
+// let age = 20;
+// let question = "Do I like coding?"
+// let likesCoding = true;
+// console.log("Hello, " + info1 + " " + name + ","+ " " + info2 + " " + age + " " + "years old. " + question + " " + likesCoding)
 
+
+// let age = 20;
+// age = 15;
+// if (age >= 18) {
+//     console.log("You are an adult!")
+// }
+// else if (age < 18) {
+//     console.log("You are not an adult yet.")
+// }
+
+
+let number = 21; 
+if (number % 2 === 0){
+    console.log("The number is even.")
+}
+else if (number % 3 === 0) {
+    console.log("The number is odd.")
+}
