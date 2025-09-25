25ad84298d8c65aba7ec9f860f267d15b1d7af88
Ann-Codewriter <qantoinnette23@gmail.com>
2025-09-24 19:46:35 -0700

    Add grades.

--- js/studentReportCard.js
+++ js/studentReportCard.js
@@ -7,3 +7,21 @@ let student = {
 };
 
 console.log(student);
+
+//Task 2
+
+if(student.grade >= 90){
+    console.log("A")
+}
+else if(student.grade >= 80){
+    console.log("B")
+}
+else if(student.grade >= 70){
+    console.log("C")
+}
+else if (student.grade >= 60){
+    console.log("D")
+}
+else {
+    console.log("F")
+}
