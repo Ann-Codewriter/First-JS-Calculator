28ce38131cd8376136c5b30b1674933509ebd6d1
Ann-Codewriter <qantoinnette23@gmail.com>
2025-09-30 14:01:54 -0700

    Add a number checker.

---
+++ loopArrays.js
@@ -0,0 +1,11 @@
+//Task 1 - Number Checker
+let num = 1;
+if(num > 0){
+    console.log("Positive number.")
+}
+else if (num < 0){
+    console.log("Negative number.");
+}
+else if (num === 0){
+    console.log("The number is zero.");
+}
