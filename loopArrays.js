4b3e19fc3bf4f4ef2dc9ae85a28661b1d5e8ba74
Ann-Codewriter <qantoinnette23@gmail.com>
2025-09-30 14:21:57 -0700

    Use a for ...in loop to print price of each item.

--- loopArrays.js
+++ loopArrays.js
@@ -9,3 +9,17 @@ else if (num < 0){
 else if (num === 0){
     console.log("The number is zero.");
 }
+
+//Task 2 - Shopping Cart (Objects + Loops)
+
+let cart = {
+    fish: 10,
+    salt: 2,
+    oil: 3
+};
+for (let key in cart) { //Number 1
+    console.log(key, "=", cart[key]);
+}
+
+let total = 0; //Number 2
+for (let i = 0; i < cart.length; i++){}
