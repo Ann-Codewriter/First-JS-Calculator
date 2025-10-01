1d52c1e24e0ca1a20c9f6a003b5b848f2460df54
Ann-Codewriter <qantoinnette23@gmail.com>
2025-10-01 13:34:11 -0700

    Change to traditional for loop.

--- js/loopArrays.js
+++ js/loopArrays.js
@@ -1,42 +1,34 @@
 //Task 1 - Number Checker
-let num = 1;
+let num = 0;
 if(num > 0){
     console.log("Positive number.")
 }
 else if (num < 0){
     console.log("Negative number.")
 }
-else if (num === 0){
+else {
     console.log("The number is zero.")
 }
 
 
 
-//Task 2 - Shopping Cart (Objects + Loops)
+// Task 2 - Shopping Cart (Objects + Loops)
 
 let cart = {
     fish: 10,
-    salt: 2,
-    oil: 3
+    salt: 7,
+    oil: 9
 };
 for (let key in cart) { //Number 1
     console.log(key, "=", cart[key]);
 }
 
-
-
 //Task 2 = Calculating the total cost of all items - Number 2
-
-let cart = {
-    fish: 10,
-    salt: 2,
-    oil: 3
-};
 
 let total = 0;
 
-for (let value of Object.values(cart)){
-    total += value;
+for (let key in cart){
+    total += cart[key];
 }
 console.log(total);
 
@@ -57,32 +49,31 @@ let employees = [ //Number 1
     {name:"Esi", age: 28, salary: 1500}
 ];
 
-for (let key in employees){ //Task 3 - Number 2
-    console.log(key, ":", employees[key]);
-}
-
-if (employees.salary >= 1000){
-    console.log("Good salary")
-}
-else {
-    console.log("Needs review")
-}
-
-let highestSalary = 0; //Number 3
+let highestSalary = 0;
 
 for (let i = 0; i < employees.length; i++){
-    if (employees[i].salary > highestSalary){
-        highestSalary = employees[i].salary;
-    }
-}
+    console.log(employees[i].name,":", employees[i].salary);
 
-console.log("Esi's Salary is", highestSalary);
+    if(employees[i].salary >= 1000){
+        console.log("Good salary")
+        }
+        else {
+            console.log("Needs review")
+        }
+        console.log("");
+
+        if (employees[i].salary > highestSalary){
+            highestSalary = employees[i].salary;
+        }
+    }
+
+     console.log("Esi's salary is", highestSalary);
 
 
 //Number 4
 let totalSalaries = 0;
 
-for (let i = 0; i < employees. length; i++){
+for (let i = 0; i < employees.length; i++){
     totalSalaries += Number(employees[i].salary);  
 }
 
